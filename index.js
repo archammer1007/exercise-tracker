94a17bf4272a1f178f43b39e3329e4ed437f774b
Ben Martini <benmartini7C0@gmail.com>
2024-07-08 12:09:47 +0000

    initial commit

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const bodyParser = require('body-parser')
+
+let mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI, {useUnifiedTopology: true});
+
+const Schema = mongoose.Schema;
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
