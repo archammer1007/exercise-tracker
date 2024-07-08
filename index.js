c756f0442d9506544206a675588b215884787b6a
Ben Martini <benmartini7C0@gmail.com>
2024-07-08 12:41:54 +0000

    feat: add database functionality for users

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ let mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URI, {useUnifiedTopology: true});
 
-const Schema = mongoose.Schema;
-
 app.use(cors())
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: false}));
@@ -18,9 +16,27 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+const Schema = mongoose.Schema;
 
+const userSchema = new Schema({
+  username: String
+});
 
+let User = mongoose.model("User", userSchema);
 
+app.post("/api/users", (req, res) => {
+  let username = req.body.username;
+  let newUser = new User({username: username});
+  newUser.save();
+  res.json(newUser);
+})
+
+app.get("/api/users", (req, res) => {
+  User.find({}, '_id username', function(err, data){
+    if (err) return console.error(err);
+    res.json(data);
+  })
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
