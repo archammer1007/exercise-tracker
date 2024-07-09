c1989f5384db68a43d69c7bac4ce4340d561cedc
Ben Martini <benmartini7C0@gmail.com>
2024-07-09 03:10:01 +0000

    feat: post exercises

--- index.js
+++ index.js
@@ -19,11 +19,20 @@ app.get('/', (req, res) => {
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: String
+  username: {type: String, required: true}
 });
 
-let User = mongoose.model("User", userSchema);
+const exerciseSchema = new Schema({
+  userid: {type: String, required: true},
+  description: {type: String, required: true},
+  duration: {type: Number, required: true},
+  date: {type: Date, default: new Date()}
+})
 
+let User = mongoose.model("User", userSchema);
+let Exercise = mongoose.model("Exercise", exerciseSchema);
+
+//save a user to the database and return the resulting object to the user
 app.post("/api/users", (req, res) => {
   let username = req.body.username;
   let newUser = new User({username: username});
@@ -31,11 +40,54 @@ app.post("/api/users", (req, res) => {
   res.json(newUser);
 })
 
+//gets a list of all users in the database with their ids
 app.get("/api/users", (req, res) => {
   User.find({}, '_id username', function(err, data){
     if (err) return console.error(err);
     res.json(data);
   })
+})
+
+//post a new exercise to the database
+app.post("/api/users/:_id/exercises", (req, res) => {
+  //first get all the info
+  let userid = req.params._id;
+  let description = req.body.description;
+  let duration = req.body.duration;
+  //date is optional
+  let date;
+  if (req.body.date){
+    date = req.body.date;
+  }
+
+  //check if the user is in the database - post fails if user is not found
+  User.findById(userid, function(err, user){
+    if (err) return console.error(err);
+    //console.log(user);
+
+    //create a new exercise and save it to the database
+    //if required fields are missing, the post fails
+    let newExercise = new Exercise({
+      userid: userid,
+      description: description,
+      duration: duration,
+      date: date
+    })
+
+    newExercise.save(function(err, exercise){
+      if (err) return console.error(err);
+      //if the post succeeds, send required information back to user in json format
+      res.json({
+        username: user.username,
+        description: exercise.description,
+        duration: exercise.duration,
+        date: exercise.date.toDateString(),
+        _id: user._id
+      })
+    });
+
+  });
+  
 })
 
 const listener = app.listen(process.env.PORT || 3000, () => {
